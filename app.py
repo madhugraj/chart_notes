9a714dfce0758d30d3b5a79594e9e865237661cb
Madhu Rajesh <madhugraj@gmail.com>
2024-09-20 17:27:34 +0530

    app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ st.markdown(
 
 # Display heading with color bar
 htp="logo1.png"
-st.image(htp, width=100)
+st.image(htp, width=200)
 st.markdown('<div style="color: green;" class="heading">Smart Chart Notes</div>', unsafe_allow_html=True)
 st.markdown('<div class="color-bar"></div>', unsafe_allow_html=True)
 
